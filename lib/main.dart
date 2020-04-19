7d1bc681cfca91503e8fbcf942178cee48700667
wonwoo <wow89@cizion.com>
2020-04-20 00:16:14 +0900

    CrossAxisAlignment.baseline

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('MainAxisAlignment.spaceAround'),
+          title: Text('CrossAxisAlignment.baseline'),
         ),
-        body: Column(
+        // Row and Column - MainAxisAlignment
+        /*body: Column(
           children: <Widget>[
             Row(
-//              mainAxisAlignment: MainAxisAlignment.start, // Row and Column - MainAxisAlignment Step 1
-//              mainAxisAlignment: MainAxisAlignment.center, // Row and Column - MainAxisAlignment Step 2
-//              mainAxisAlignment: MainAxisAlignment.end, // Row and Column - MainAxisAlignment Step 3
-//              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Row and Column - MainAxisAlignment Step 4
-//              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Row and Column - MainAxisAlignment Step 5
-              mainAxisAlignment: MainAxisAlignment.spaceAround, // Row and Column - MainAxisAlignment Step 6
+//              mainAxisAlignment: MainAxisAlignment.start, // Step 1
+//              mainAxisAlignment: MainAxisAlignment.center, // Step 2
+//              mainAxisAlignment: MainAxisAlignment.end, // Step 3
+//              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Step 4
+//              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Step 5
+              mainAxisAlignment: MainAxisAlignment.spaceAround, // Step 6
               children: <Widget>[
                 Text('Row'),
                 Icon(Icons.star, size: 50),
@@ -29,18 +30,32 @@ class MyApp extends StatelessWidget {
             ),
             Text(''),
             Column(
-//              mainAxisAlignment: MainAxisAlignment.start, // Row and Column - MainAxisAlignment Step 1
-//              mainAxisAlignment: MainAxisAlignment.center, // Row and Column - MainAxisAlignment Step 2
-//              mainAxisAlignment: MainAxisAlignment.end, // Row and Column - MainAxisAlignment Step 3
-//              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Row and Column - MainAxisAlignment Step 4
-//              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Row and Column - MainAxisAlignment Step 5
-              mainAxisAlignment: MainAxisAlignment.spaceAround, // Row and Column - MainAxisAlignment Step 6
+//              mainAxisAlignment: MainAxisAlignment.start, // Step 1
+//              mainAxisAlignment: MainAxisAlignment.center, // Step 2
+//              mainAxisAlignment: MainAxisAlignment.end, // Step 3
+//              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Step 4
+//              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Step 5
+              mainAxisAlignment: MainAxisAlignment.spaceAround, // Step 6
               children: <Widget>[
                 Text('Column'),
                 Icon(Icons.star, size: 50),
                 Icon(Icons.star, size: 50),
                 Icon(Icons.star, size: 50),
               ],
+            ),
+          ],
+        ),*/
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.baseline,  // Row and Column - CrossAxisAlignment Step 1
+          textBaseline: TextBaseline.alphabetic,            // Row and Column - CrossAxisAlignment Step 1
+          children: <Widget>[
+            Text(
+              'Baseline',
+              style: Theme.of(context).textTheme.display3,
+            ),
+            Text(
+              'Baseline',
+              style: Theme.of(context).textTheme.body1,
             ),
           ],
         ),
