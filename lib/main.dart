586c085f2e7598eb7d469f62549df8429052481f
wonwoo <wow89@cizion.com>
2020-04-19 23:58:16 +0900

    MainAxisAlignment.spaceEvenly

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('MainAxisAlignment.spaceBetween'),
+          title: Text('MainAxisAlignment.spaceEvenly'),
         ),
         body: Column(
           children: <Widget>[
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
 //              mainAxisAlignment: MainAxisAlignment.start, // Row and Column - MainAxisAlignment Step 1
 //              mainAxisAlignment: MainAxisAlignment.center, // Row and Column - MainAxisAlignment Step 2
 //              mainAxisAlignment: MainAxisAlignment.end, // Row and Column - MainAxisAlignment Step 3
-              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Row and Column - MainAxisAlignment Step 4
+//              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Row and Column - MainAxisAlignment Step 4
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Row and Column - MainAxisAlignment Step 5
               children: <Widget>[
                 Text('Row'),
                 Icon(Icons.star, size: 50),
@@ -30,7 +31,8 @@ class MyApp extends StatelessWidget {
 //              mainAxisAlignment: MainAxisAlignment.start, // Row and Column - MainAxisAlignment Step 1
 //              mainAxisAlignment: MainAxisAlignment.center, // Row and Column - MainAxisAlignment Step 2
 //              mainAxisAlignment: MainAxisAlignment.end, // Row and Column - MainAxisAlignment Step 3
-              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Row and Column - MainAxisAlignment Step 4
+//              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Row and Column - MainAxisAlignment Step 4
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Row and Column - MainAxisAlignment Step 5
               children: <Widget>[
                 Text('Column'),
                 Icon(Icons.star, size: 50),
