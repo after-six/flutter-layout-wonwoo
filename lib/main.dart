29698afae99a4084f994080cc70598766777f83f
wonwoo <wow89@cizion.com>
2020-04-22 17:17:19 +0900

    Silvers SilverList

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,29 @@ class MyApp extends StatelessWidget {
         ),
         body: CustomScrollView(
           slivers: [
+            SliverList(
+              delegate: SliverChildListDelegate(const [
+                ListTile(title: Text('First item')),
+                ListTile(title: Text('Second item')),
+                ListTile(title: Text('Third item')),
+                ListTile(title: Text('Fourth item')),
+              ]),
+            ),
             SliverFillRemaining(
               hasScrollBody: false,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  FlutterLogo(size: 200),
-                  Text(
-                    'This is some longest text that should be centered'
-                        'together with the logo',
-                    textAlign: TextAlign.center,
-                  ),
-                ],
+              child: Container(
+                color: Colors.yellowAccent,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    FlutterLogo(size: 200),
+                    Text(
+                      'This is some longest text that should be centered'
+                          'together with the logo',
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
