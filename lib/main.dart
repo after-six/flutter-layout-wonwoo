37e4c25f1a270da2e2a1bc43c145f3ac9f717e95
wonwoo <wow89@cizion.com>
2020-04-22 16:48:05 +0900

    Stack Layout LayoutBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const double iconSize = 50;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Stack'),
         ),
-        body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Material(color: Colors.yellowAccent),
-            Positioned(
-              top: 0,
-              left: 0,
-              child: Icon(Icons.star, size: 50),
-            ),
-            Positioned(
-              top: 340,
-              left: 250,
-              child: Icon(Icons.call, size: 50),
-            ),
-          ],
+        body: LayoutBuilder(
+          builder: (context, constraints) =>
+              Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Material(color: Colors.yellowAccent),
+                  Positioned(
+                    top: 0,
+                    child: Icon(Icons.star, size: iconSize),
+                  ),
+                  Positioned(
+                    top: constraints.maxHeight - iconSize,
+                    left: constraints.maxWidth - iconSize,
+                    child: Icon(Icons.call, size: iconSize),
+                  ),
+                ],
+              ),
         ),
       )
     );
