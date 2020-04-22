5120acd6013cdf886011c7edd83c0fbd235c6072
wonwoo <wow89@cizion.com>
2020-04-22 16:46:33 +0900

    Stack Layout Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -7,32 +7,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Scaffold(
-            appBar: AppBar(
-              title: Text('Stack'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Stack'),
+        ),
+        body: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Material(color: Colors.yellowAccent),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Icon(Icons.star, size: 50),
             ),
-          ),
-          Banner(
-            message: "Top Start",
-            location: BannerLocation.topStart,
-          ),
-          Banner(
-            message: "Top End",
-            location: BannerLocation.topEnd,
-          ),
-          Banner(
-            message: "Bottom Start",
-            location: BannerLocation.bottomStart,
-          ),
-          Banner(
-            message: "Bottom End",
-            location: BannerLocation.bottomEnd,
-          ),
-        ],
-      ),
+            Positioned(
+              top: 340,
+              left: 250,
+              child: Icon(Icons.call, size: 50),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
