5f499b1cf9411269f009eb501697dcf747bdc453
wonwoo <wow89@cizion.com>
2020-04-22 16:54:52 +0900

    Expanded

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,26 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('MainAxisAlignment.start'),
         ),
-        body: Column(
+        body: Row(
           children: <Widget>[
-
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.red),
+              ),
+              flex: 3,
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.green),
+              ),
+              flex: 2,
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.blue),
+              ),
+              flex: 1,
+            ),
           ],
         ),
       ),
